fae151c1f45a7b94fbc3c24428173bff116912e7
Seifbarouni <seif.barouni11@gmail.com>
2021-06-11 13:22:45 +0100

    added likes

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,24 +3,50 @@
   export let comments = 0;
   export let isComment = false;
   export let id;
+  let liked = false;
+  function likePost() {
+    likes++;
+    liked = true;
+  }
+
+  function dislikePost() {
+    likes--;
+    liked = false;
+  }
 </script>
 
 <div id="footer" class="flex items-center pt-3">
-  <span class="px-1">
-    <svg
-      class="w-4 h-4 hover:fill-current hover:text-yellow-400"
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      ><path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-      /></svg
-    >
-  </span>
+  {#if liked == false}
+    <a href="#/" class="px-1" on:click={likePost}>
+      <svg
+        class="w-4 h-4 hover:fill-current hover:text-yellow-400"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        ><path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+        /></svg
+      >
+    </a>
+  {:else}
+    <a href="#/" class="px-1" on:click={dislikePost}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-4 h-4 fill-current text-yellow-400"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+        />
+      </svg>
+    </a>
+  {/if}
+
   <span class="text-sm  md:text-xs">{likes}</span>
   {#if !isComment}
     <span class="flex text-sm items-center px-2 md:text-xs">
